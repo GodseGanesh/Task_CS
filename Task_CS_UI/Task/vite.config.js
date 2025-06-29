ef84b2768fa083b3f4441c5d84429fe0097505a8
GodseGanesh <ganeshgodse1902@gmail.com>
2025-06-29 18:26:10 +0530

    chnages in compose  file

--- Task_CS_UI/Task/vite.config.js
+++ Task_CS_UI/Task/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true
+    host: true,           
+    port: 5173,           
+    strictPort: true,     
+    watch: {
+      usePolling: true    
+    }
   }
 })
